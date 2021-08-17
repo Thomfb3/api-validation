71f310a22e88c37851a4c74ca7cba609206e659b
Thomas Balunis <thomfb3@gmail.com>
2021-08-17 00:45:54 -0400

    Part 1 complete

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Book = require("../models/book");
 
+const ExpressError = require("../expressError")
+
+const jsonschema = require("jsonschema");
+const bookSchema = require("../schemas/bookSchema.json");
+
 const router = new express.Router();
 
 
@@ -29,7 +34,18 @@ router.get("/:id", async function (req, res, next) {
 /** POST /   bookData => {book: newBook}  */
 
 router.post("/", async function (req, res, next) {
-  try {
+
+  const result = jsonschema.validate(req.body, bookSchema);
+
+  if (!result.valid) {
+    // pass validation errors to error handler
+    //  (the "stack" key is generally the most useful)
+    let listOfErrors = result.errors.map(error => error.stack);
+    let error = new ExpressError(listOfErrors, 400);
+    return next(error);
+  }
+
+  try {    
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
@@ -40,6 +56,17 @@ router.post("/", async function (req, res, next) {
 /** PUT /[isbn]   bookData => {book: updatedBook}  */
 
 router.put("/:isbn", async function (req, res, next) {
+
+  const result = jsonschema.validate(req.body, bookSchema);
+
+  if (!result.valid) {
+    // pass validation errors to error handler
+    //  (the "stack" key is generally the most useful)
+    let listOfErrors = result.errors.map(error => error.stack);
+    let error = new ExpressError(listOfErrors, 400);
+    return next(error);
+  }
+
   try {
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
@@ -51,6 +78,7 @@ router.put("/:isbn", async function (req, res, next) {
 /** DELETE /[isbn]   => {message: "Book deleted"} */
 
 router.delete("/:isbn", async function (req, res, next) {
+
   try {
     await Book.remove(req.params.isbn);
     return res.json({ message: "Book deleted" });
